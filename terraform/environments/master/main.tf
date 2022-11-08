bf5567a6318f8aaed62d9230d8f11b837d85e2b0
Guthikonda Rakesh <guthikonda.rakesh@springml.com>
2022-11-08 18:36:29 +0530

    first commit

---
+++ terraform/environments/master/main.tf
@@ -0,0 +1,30 @@
+provider "google" {
+  region      = "${var.region}"
+}
+
+resource "google_project" "giridhar_project" {
+  name       = "giridhar-project"
+  project_id = "giridhar-project-1"
+
+  org_id = "${var.gsuite_org_id}"
+  billing_account = "${var.billing_account_id}"
+}
+
+resource "google_project_iam_binding" "gsuite_project_owner" {
+  project = "${google_project.giridhar_project.project_id}"
+  role    = "roles/owner"
+
+  members = [
+    "user:${var.gsuite_user}",
+    "user:${var.billing_account_user}",
+  ]
+}
+
+
+# resource "google_billing_account_iam_binding" "editor" {
+#   billing_account_id = "${var.billing_account_id}"
+#   role               = "roles/billing.user"
+#   members = [
+#     "user:banothuramu@gnimmagadda.com",
+#   ]
+# }
---
+++ terraform/environments/master/terraform.tfvars
@@ -0,0 +1,6 @@
+billing_account_user = "giridharn@gnimmagadda.com"
+billing_folder_id = "729227862814" # my-billing-folder
+billing_account_id = "010DDC-D72190-F08FDC" # my-billing-account
+
+gsuite_user = "giridharn@gnimmagadda.com"
+gsuite_org_id = "729227862814"
